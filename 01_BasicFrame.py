1efea7e2473dcba44d721dbfe5f45cb6446a6f3a
Spritecodej <spritecodej@gmail.com>
2018-07-20 00:39:43 +0900

    fix.. a little, just overhaul

--- 01_BasicFrame.py
+++ 01_BasicFrame.py
@@ -1,11 +1,30 @@
+
 import wx
+from six import print_
 
 class iFrame(wx.Frame):
-    def __init__(self, parent, id):
-        wx.Frame.__init__(self, parent, id, 'App Tester', size=(300,200))
+    def __init__(self, *args, **kw):
+        wx.Frame.__init__(self, *args, **kw)
+        self.Bind(wx.EVT_SIZE, self.onSize)
+        wx.CallAfter(self.after, 1, 2, 3)
+        
+    def after(self, a, b, c):
+        print_('Called via wx.CallAfter : ', a, b, c)
+
+    def onSize(self, evt):
+        print_(repr(evt.Size))
+        evt.Skip()
+
+class iApp(wx.App):
+    def OnInit(self):
+        frm = iFrame(None, title="Hello, WxPython", size=(480,360))
+        frm.Show()
+        return True
+    
+    def OnExit(self):
+        print_('OnExit')
+        return 0
 
 if __name__ == '__main__':
-    app = wx.App(False)
-    frame = iFrame(parent=None, id=-1)
-    frame.Show()
+    app = iApp()
     app.MainLoop()
--- 02_CreatingButtons.py
+++ 02_CreatingButtons.py
@@ -5,12 +5,22 @@ class iFrame(wx.Frame):
     def __init__(self, parent, id):
         wx.Frame.__init__(self, parent, id, 'App Tester', size=(300,200))
         panel = wx.Panel(self)
-        button = wx.Button(panel, label="exit", pos=(130,10), size=(60,60))
-        self.Bind(wx.EVT_BUTTON, self.closebutton, button)
-        self.Bind(wx.EVT_CLOSE, self.closewindow)
+        
+        button_start = wx.Button(panel, label="start", pos=(30,10), size=(60,60))
+        self.Bind(wx.EVT_BUTTON, self.startbutton, button_start)
+        button_exit = wx.Button(panel, label="exit", pos=(130,10), size=(60,60))
+        self.Bind(wx.EVT_BUTTON, self.closebutton, button_exit)
+
+
+        self.itext = wx.StaticText(panel, -1, "", (10,80), (260,-1), wx.ALIGN_CENTER)
+        
+        #self.Bind(wx.EVT_CLOSE, self.closewindow)
 
     def closebutton(self, event):
         self.Close(True)
+
+    def startbutton(self, event):
+        self.itext.SetLabel("Hello")
 
     def closewindow(self, event):
         self.Destroy()
